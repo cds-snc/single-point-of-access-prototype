fb43317c490efa64034842193aa1416cdaf62752
smcmurtry <smcmurtry@gmail.com>
2019-10-25 16:02:11 -0600

    hooked up the gsin autocomplete

--- routes/search/search.controller.js
+++ routes/search/search.controller.js
@@ -22,22 +22,37 @@ const sortPostingDate = array => {
 module.exports = (app, route) => {
   const name = route.name
 
-  const sampleData = fs.readFileSync('data/sample_data.json')
-  const sampleData2 = JSON.parse(sampleData)
+  const _sampleData = fs.readFileSync('data/sample_data.json')
+  const sampleData = JSON.parse(_sampleData)
+
+  const filterByProvince = (array, selectedProvinces) => {
+    if(selectedProvinces.size < 1) {
+      return array
+    }
+    return array.filter(x => {
+      const listedProvinces = new Set(x.province_territory_of_work)
+      if (intersection(listedProvinces, selectedProvinces).size > 0) {
+        return true
+      }
+        return false
+    })
+  }
+
+  const filterByGsin = (array, selectedGsins) => {
+    if(selectedGsins.length < 1) {
+      return array
+    }
+    const gsin = selectedGsins[0] // there is only 1 for now
+    return array.filter(x => {
+      const matchingElements = x.GSIN_code.filter(listingGsin => {
+        const firstChars = listingGsin.slice(0, gsin.length)
+        return firstChars === gsin
+      })
+      return matchingElements.length > 0
+    })
+  }
 
   route.draw(app).get((req, res) => {
-    const filter = (array, selectedProvinces) => {
-      if(selectedProvinces.size < 1) {
-        return array
-      }
-      return array.filter(x => {
-        const listedProvinces = new Set(x.province_territory_of_work)
-        if (intersection(listedProvinces, selectedProvinces).size > 0) {
-          return true
-        }
-          return false
-      })
-    }
 
     let category = ' All categories'
 
@@ -49,8 +64,19 @@ module.exports = (app, route) => {
     if (req.query.filters) {
       provTerr = req.query.filters
     }
+
+    let gsin = ""
+    if (req.query.gsin) {
+      gsin = req.query.gsin
+    }
+
+
+
     const selectedProvinces = new Set(provTerr.split(",").filter(x => x))
-    const results = filter(sampleData2, selectedProvinces)
+    const selectedGsins = gsin.split(",").filter(x => x)
+    const _results = filterByGsin(sampleData, selectedGsins)
+    const results = filterByProvince(_results, selectedProvinces)
+
     res.render(
       name,
       routeUtils.getViewData(req, { 
